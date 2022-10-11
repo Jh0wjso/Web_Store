5313087cc7509c8d20ca8e70a133b45c0b841e3d
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-10-11 18:13:41 -0300

    Adding Home Files

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
 
-    <div className="MainContent h-100">
+    <div className="MainContent h-100 d-flex flex-column">
       <div className='w-screen container'>
         <Header />
+      </div>
+      <div>
+        <Home />
       </div>
     </div>
   );
