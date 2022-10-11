80d22b065484dc06ce033c1acc85ad7ef4955a22
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-10-11 18:03:57 -0300

    Header File

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,29 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Header from './components/Header';
 
 function App() {
   return (
 
     <div className="MainContent h-100">
-      <div className='d-flex justify-content-center w-screen container'>
-        <Navbar collapseOnSelect expand="lg" bg="#383737" variant="dark">
-          <Container>
-            <Navbar.Brand href="App.js">Stylos</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#">Feminino</Nav.Link>
-                <Nav.Link href="#">Masculino</Nav.Link>
-                <Nav.Link href="#">Perfumes</Nav.Link>
-                <Nav.Link href="#">Contato</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+      <div className='w-screen container'>
+        <Header />
       </div>
     </div>
   );
---
+++ Client/src/components/Header/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Container from "react-bootstrap/esm/Container";
+import Nav from "react-bootstrap/esm/Nav";
+import Navbar from "react-bootstrap/esm/Navbar";
+
+export default function Header(){
+    return(
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="#383737" variant="dark" fixed='top'>
+                <Container className="d-flex justify-content-between align-items-center">
+                    <Navbar.Brand href="App.js">
+                    <h1>Stylos</h1>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#">Feminino</Nav.Link>
+                        <Nav.Link href="#">Masculino</Nav.Link>
+                        <Nav.Link href="#">Perfumes</Nav.Link>
+                        <Nav.Link href="#">Contato</Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+}
