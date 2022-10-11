8459e1811f36faeff3c3fc7b61e228287e38c0a8
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-10-10 22:19:22 -0300

    Removing unused files

--- Client/src/pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-import { Header } from "../components/Header/Header"
-
-export function Index(){
-    return(
-        <div className="flex flex-col">
-            <Header/>
-        </div>
-    )
-}
