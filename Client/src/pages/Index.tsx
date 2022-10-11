58abf7e0bf57b1890c46efc98bbcb6ba602d4c55
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-10-10 21:55:25 -0300

    React Project

---
+++ Client/src/pages/Index.tsx
@@ -0,0 +1,9 @@
+import { Header } from "../components/Header/Header"
+
+export function Index(){
+    return(
+        <div className="flex flex-col">
+            <Header/>
+        </div>
+    )
+}
