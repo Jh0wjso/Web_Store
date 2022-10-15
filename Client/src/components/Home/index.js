d3352e60b0faf504ee8bfdf83ae34993c0bf63e6
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-10-15 11:49:24 -0300

    New Home Component

--- Client/src/components/Home/index.js
+++ Client/src/components/Home/index.js
@@ -3,32 +3,10 @@ import "./styles.css";
 
 import { MDBIcon } from "mdb-react-ui-kit";
 
-/*
-<div className="container mb-3">
-        <div className="maincontent text-justify pt-3 container d-flex flex-row align-items-center justify-content-between">
-          <div className="textStore">
-            <h3>Quem somos nos</h3>
-            <p>Prazer sou ..... aqui voce conhecera melhor sobre nossa loja e nossos produtos</p>
-          </div>
-          <img src={storeModel} className="imgStore" width={350} height={350} />
-        </div>
-      </div>
-
-      <MyCarousel />
-
-      <div className="emailSender container">
-          <span>Deixe seu email</span>
-          <div>
-            <input placeholder="email@exemplo.com" />
-            <button>Enviar</button>
-          </div>
-        </div>
-*/
-
 export default function Home() {
   return (
-    <section className="homeContent pt-4 border-bottom d-flex flex-column justify-content-between">
-      <div className="p- text-center">
+    <section className="homeContent mt-5 border-bottom d-flex flex-column justify-content-between">
+      <section className="text-center">
         <h1 className="font-weight-bold">
           Nome Loja
         </h1>
@@ -43,7 +21,7 @@ export default function Home() {
             <MDBIcon icon="map-marker" className="ml-1" />
           </div>
         </div>
-      </div>
+      </section>
     </section>
   );
 }
