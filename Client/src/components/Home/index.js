71a5f038d9eec85e5aaeb3c1fcecc3cbb49f4950
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-10-15 17:57:21 -0300

    Update on texts

--- Client/src/components/Home/index.js
+++ Client/src/components/Home/index.js
@@ -8,12 +8,12 @@ export default function Home() {
     <section className="homeContent mt-5 border-bottom d-flex flex-column justify-content-between">
       <section className="text-center">
         <h1 className="font-weight-bold">
-          Nome Loja
+          Nome da Loja
         </h1>
         <div className="d-flex flex-row text-center justify-content-center">
           <div className="text-center mr-3">
             <MDBIcon icon="phone" className="mr-1"/>
-            <span>(14) 99798-1721</span>
+            <span>(XX) XXXXX-XXXX</span>
           </div>
           <p className="divider">|</p>
           <div className="text-center ml-3 md:ml-3">
