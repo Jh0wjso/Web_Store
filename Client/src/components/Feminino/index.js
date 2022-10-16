bafe7b948f8bbc56a0e70919fc474f65372a0d53
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-10-15 22:19:35 -0300

    New carousel version

--- Client/src/components/Feminino/index.js
+++ Client/src/components/Feminino/index.js
@@ -1,13 +1,62 @@
 import React from "react";
 import "./styles.css";
 
-export default function Feminino()
-{
-    return(
-        <div>
-            <span>
-                Conteudo feminino aqui!
-            </span>
-        </div>
-    );
+import Carousel from "better-react-carousel";
+
+export default function Feminino() {
+  return (
+    <div className="container">
+        <h2
+            className="mb-3"
+        >
+            Roupas Femininas
+        </h2>
+      <section>
+        <Carousel cols={3} rows={1} gap={10} loop>
+          <Carousel.Item>
+            <img
+              width="100%"
+              src="https://picsum.photos/800/600?random=1"
+              className="rounded"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              width="100%"
+              src="https://picsum.photos/800/600?random=2"
+              className="rounded"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              width="100%"
+              src="https://picsum.photos/800/600?random=1"
+              className="rounded"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              width="100%"
+              src="https://picsum.photos/800/600?random=2"
+              className="rounded"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              width="100%"
+              src="https://picsum.photos/800/600?random=1"
+              className="rounded"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              width="100%"
+              src="https://picsum.photos/800/600?random=2"
+              className="rounded"
+            />
+          </Carousel.Item>
+        </Carousel>
+      </section>
+    </div>
+  );
 }
