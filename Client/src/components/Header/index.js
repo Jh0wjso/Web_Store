6abcb621ba0afe32e257bd33a4a84f68b8014a40
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-10-18 17:25:55 -0300

    New header

--- Client/src/components/Header/index.js
+++ Client/src/components/Header/index.js
@@ -6,26 +6,31 @@ import "./styles.css";
 
 export default function Header() {
   return (
-    <div className="content-header d-flex justify-content-start">
-      <Navbar expand="lg" variant="dark">
-        <Container className="d-flex flex-row align-items-center justify-content-between">
-          <Navbar.Brand href="#">
-            <h1>
-              <strong>Nome</strong>
-            </h1>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#feminino">Feminino</Nav.Link>
-              <Nav.Link href="#man">Masculino</Nav.Link>
-              <Nav.Link href="#beauty">Beleza</Nav.Link>
-              <Nav.Link href="#">Sobre</Nav.Link>
-              <Nav.Link href="#">Contato</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+    <Navbar 
+      expand="lg"
+      className="content-header"
+      variant="dark"
+      style={{backgroundColor: "transparent"}}
+    >
+      <Container className="d-flex flex-row justify-content-between">
+        <Navbar.Brand href="#">
+          <h1>
+            <strong className="nameStore">
+              Nome
+            </strong>
+          </h1>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#feminino">Feminino</Nav.Link>
+            <Nav.Link href="#man">Masculino</Nav.Link>
+            <Nav.Link href="#beauty">Beleza</Nav.Link>
+            <Nav.Link href="#">Sobre</Nav.Link>
+            <Nav.Link href="#">Contato</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
