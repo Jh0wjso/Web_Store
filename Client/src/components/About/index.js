a610b4137c74722b5ebc43dd7ef4a648e0f8bb21
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-10-25 19:46:48 -0300

    Update on AboutComponent

--- Client/src/components/About/index.js
+++ Client/src/components/About/index.js
@@ -3,16 +3,16 @@ import "./styles.css";
 
 export default function About() {
   return (
-    <div className="fw-bold d-flex flex-row justify-content-between container mb-5">
-      <section className="content-left justify-content-between p-5">
-        <h1 className="text-center font-weight-bold">Stylos Store</h1>
-        <span className="text-center font-weight-bold">Conheça mais sobre a nosso loja.</span>
+    <div className="fullContainer fw-bold d-flex justify-content-between mb-3 mx-3 rounded">
+      <section className="content-left d-flex justify-content-center align-items-center">
+        <h1 className="text-center font-weight-bold">Stylos <br/><h1 className="ml-5">Store</h1></h1>
       </section>
-      <div className="border-right borda" />
+      <div className="border-right m-4 borda" />
+      <div className="border-bottom m-4 bordaBottom" />
       <section className="content-right p-5">
         <h1 className="text-center font-weight-bold">Sobre Nos</h1>
         <div className="container">
-          <strong className="pcText text-justify">
+          <span className="pcText text-justify">
             Prazer sou Daiane, sou natural de Tejupa, e fico feliz por voce
             estar aqui acessando meu site, espero que tenha gostado da
             experiencia. Vou contar um pouco sobre minha loja, nos somos uma
@@ -20,14 +20,14 @@ export default function About() {
             para Homens e Mulheres alem de produtos de beleza para ambos, voce
             pode nos encontrar em varias midias sociais, aqui a baixo voce pode
             ter acesso a todas as nossas midias.
-          </strong>
-          <strong className="mobileText">
+          </span>
+          <span className="mobileText">
             Vou contar um pouco sobre minha loja, nos somos uma loja que vende
             roupas a mais de 10 anos, todos os tipos de roupas para Homens e
             Mulheres alem de produtos de beleza para ambos, voce pode nos
             encontrar em varias midias sociais, aqui a baixo voce pode ter
             acesso a todas as nossas midias.
-          </strong>
+          </span>
         </div>
       </section>
     </div>
