7a341e031f9b91391d74b6455eaf22ca617439dd
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-11-08 12:27:09 -0300

    Mans products

--- Store/src/components/Male/index.js
+++ Store/src/components/Male/index.js
@@ -9,6 +9,12 @@ import Tres from "../../assets/Products/Roupas/Masculinas/3.jpeg";
 import Quatro from "../../assets/Products/Roupas/Masculinas/4.jpeg";
 import Cinco from "../../assets/Products/Roupas/Masculinas/5.jpeg";
 import Seis from "../../assets/Products/Roupas/Masculinas/6.jpeg";
+import Sete from "../../assets/Products/Roupas/Masculinas/7.jpeg";
+import Oito from "../../assets/Products/Roupas/Masculinas/8.jpeg";
+import Nove from "../../assets/Products/Roupas/Masculinas/9.jpeg";
+import Dez from "../../assets/Products/Roupas/Masculinas/10.jpeg";
+import Onze from "../../assets/Products/Roupas/Masculinas/11.jpeg";
+import Doze from "../../assets/Products/Roupas/Masculinas/12.jpeg";
 
 export default function Male() {
   return (
@@ -63,6 +69,60 @@ export default function Male() {
           </Carousel.Item>
           <Carousel.Item>
             <img width="100%" src={Seis} className="img-product" />
+            <a
+              href="https://api.whatsapp.com/send?phone=5514998074693&text=Oi%2C%20estou%20interessado(a)%20nas%20blusinhas."
+              className="btn-interesse border-0 w-100 d-flex justify-content-center p-2 text-white text-decoration-none font-weight-bold"
+            >
+              Me interesso
+            </a>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width="100%" src={Sete} className="img-product" />
+            <a
+              href="https://api.whatsapp.com/send?phone=5514998074693&text=Oi%2C%20estou%20interessado(a)%20nas%20blusinhas."
+              className="btn-interesse border-0 w-100 d-flex justify-content-center p-2 text-white text-decoration-none font-weight-bold"
+            >
+              Me interesso
+            </a>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width="100%" src={Oito} className="img-product" />
+            <a
+              href="https://api.whatsapp.com/send?phone=5514998074693&text=Oi%2C%20estou%20interessado(a)%20nas%20blusinhas."
+              className="btn-interesse border-0 w-100 d-flex justify-content-center p-2 text-white text-decoration-none font-weight-bold"
+            >
+              Me interesso
+            </a>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width="100%" src={Nove} className="img-product" />
+            <a
+              href="https://api.whatsapp.com/send?phone=5514998074693&text=Oi%2C%20estou%20interessado(a)%20nas%20blusinhas."
+              className="btn-interesse border-0 w-100 d-flex justify-content-center p-2 text-white text-decoration-none font-weight-bold"
+            >
+              Me interesso
+            </a>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width="100%" src={Dez} className="img-product" />
+            <a
+              href="https://api.whatsapp.com/send?phone=5514998074693&text=Oi%2C%20estou%20interessado(a)%20nas%20blusinhas."
+              className="btn-interesse border-0 w-100 d-flex justify-content-center p-2 text-white text-decoration-none font-weight-bold"
+            >
+              Me interesso
+            </a>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width="100%" src={Onze} className="img-product" />
+            <a
+              href="https://api.whatsapp.com/send?phone=5514998074693&text=Oi%2C%20estou%20interessado(a)%20nas%20blusinhas."
+              className="btn-interesse border-0 w-100 d-flex justify-content-center p-2 text-white text-decoration-none font-weight-bold"
+            >
+              Me interesso
+            </a>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width="100%" src={Doze} className="img-product" />
             <a
               href="https://api.whatsapp.com/send?phone=5514998074693&text=Oi%2C%20estou%20interessado(a)%20nas%20blusinhas."
               className="btn-interesse border-0 w-100 d-flex justify-content-center p-2 text-white text-decoration-none font-weight-bold"
