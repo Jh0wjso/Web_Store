e0c0aa0401704676285e057eba667cde3c1a3d76
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-11-10 23:57:49 -0300

    Updates

---
+++ precache-manifest.4a623cbc980119bf1f9ca90d23a2f817.js
@@ -0,0 +1,282 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7611ccd8d78d4fe6e57",
+    "url": "/Portifolio/static/css/main.78378618.chunk.css"
+  },
+  {
+    "revision": "c7611ccd8d78d4fe6e57",
+    "url": "/Portifolio/static/js/main.c7611ccd.chunk.js"
+  },
+  {
+    "revision": "5933624ef5c4c436412d",
+    "url": "/Portifolio/static/js/1.5933624e.chunk.js"
+  },
+  {
+    "revision": "b4e120a26871d622313a",
+    "url": "/Portifolio/static/js/2.b4e120a2.chunk.js"
+  },
+  {
+    "revision": "69b63192476b3a0ab886",
+    "url": "/Portifolio/static/js/runtime~main.69b63192.js"
+  },
+  {
+    "revision": "bde82765856804836a7af1cc94cbed5f",
+    "url": "/Portifolio/static/media/1.bde82765.jpeg"
+  },
+  {
+    "revision": "a050de907d99841288646511e4baa08f",
+    "url": "/Portifolio/static/media/2.a050de90.jpeg"
+  },
+  {
+    "revision": "7ee4f3d0462b70f9df04d589bd0b223c",
+    "url": "/Portifolio/static/media/3.7ee4f3d0.jpeg"
+  },
+  {
+    "revision": "bfed520c9888b1efb0b9a320fb6360c3",
+    "url": "/Portifolio/static/media/4.bfed520c.jpeg"
+  },
+  {
+    "revision": "a4788d8c2f9f70f8f56e4cdd7d8852af",
+    "url": "/Portifolio/static/media/5.a4788d8c.jpeg"
+  },
+  {
+    "revision": "9a25a15cd8890c7a2b96bbe1ae5bea27",
+    "url": "/Portifolio/static/media/6.9a25a15c.jpeg"
+  },
+  {
+    "revision": "b3b660a132d7d70e995f2368d90d0a37",
+    "url": "/Portifolio/static/media/7.b3b660a1.jpeg"
+  },
+  {
+    "revision": "30809827cfd904709c8ca60583a90943",
+    "url": "/Portifolio/static/media/8.30809827.jpeg"
+  },
+  {
+    "revision": "2864fcdc9b3aff454cd2f566f9472da4",
+    "url": "/Portifolio/static/media/9.2864fcdc.jpeg"
+  },
+  {
+    "revision": "dfdbf53774ec3e5361e706c34b0b9f13",
+    "url": "/Portifolio/static/media/10.dfdbf537.jpeg"
+  },
+  {
+    "revision": "d2922c5f4b9425bc50ecba3a6557eedd",
+    "url": "/Portifolio/static/media/11.d2922c5f.jpeg"
+  },
+  {
+    "revision": "f432e95679858e0d4cb575e0e944be5c",
+    "url": "/Portifolio/static/media/12.f432e956.jpeg"
+  },
+  {
+    "revision": "33027a5da9370391f99f2cc47ef2f6ec",
+    "url": "/Portifolio/static/media/13.33027a5d.jpeg"
+  },
+  {
+    "revision": "7185516c2491101ea116a2b2299be9fb",
+    "url": "/Portifolio/static/media/14.7185516c.jpeg"
+  },
+  {
+    "revision": "7b5b80e44467ddca7ee856a6de71e7b7",
+    "url": "/Portifolio/static/media/15.7b5b80e4.jpeg"
+  },
+  {
+    "revision": "358a8886ad8e3781b05cf575d3a52873",
+    "url": "/Portifolio/static/media/16.358a8886.jpeg"
+  },
+  {
+    "revision": "5949885b4483d077877a3fd6417a909f",
+    "url": "/Portifolio/static/media/17.5949885b.jpeg"
+  },
+  {
+    "revision": "ca1d8c9e5f7767175a23bc0ea2e07135",
+    "url": "/Portifolio/static/media/18.ca1d8c9e.jpeg"
+  },
+  {
+    "revision": "b97bfc53cc27878333483b25ad5d20ac",
+    "url": "/Portifolio/static/media/19.b97bfc53.jpeg"
+  },
+  {
+    "revision": "f9329571d79725a9cb305aabcd474e7d",
+    "url": "/Portifolio/static/media/20.f9329571.jpeg"
+  },
+  {
+    "revision": "36745dc60c6116cdc637d136e6698a3e",
+    "url": "/Portifolio/static/media/1.36745dc6.jpeg"
+  },
+  {
+    "revision": "84bc340e7c6ef9c2b2852fb3afe3f855",
+    "url": "/Portifolio/static/media/2.84bc340e.jpeg"
+  },
+  {
+    "revision": "4b0c09b2d954160075fb15d64562b32a",
+    "url": "/Portifolio/static/media/3.4b0c09b2.jpeg"
+  },
+  {
+    "revision": "4c08450c3397b987cc8415add7ddaf98",
+    "url": "/Portifolio/static/media/4.4c08450c.jpeg"
+  },
+  {
+    "revision": "18b913c92aaab6446b5a10e174507068",
+    "url": "/Portifolio/static/media/5.18b913c9.jpeg"
+  },
+  {
+    "revision": "da5e44acf07abb7b9badf53bf65d98a4",
+    "url": "/Portifolio/static/media/6.da5e44ac.jpeg"
+  },
+  {
+    "revision": "04b3eef138f0e00d3598f94a40ac8e9d",
+    "url": "/Portifolio/static/media/7.04b3eef1.jpeg"
+  },
+  {
+    "revision": "7a21bb72ed9e0923d41b5f7b8a27d222",
+    "url": "/Portifolio/static/media/8.7a21bb72.jpeg"
+  },
+  {
+    "revision": "1b928e4918f375d732eb219995d8a3c6",
+    "url": "/Portifolio/static/media/9.1b928e49.jpeg"
+  },
+  {
+    "revision": "ff0c6f2225edcff8b3081daed86bff32",
+    "url": "/Portifolio/static/media/10.ff0c6f22.jpeg"
+  },
+  {
+    "revision": "6a10b1f3c788efb5a4bd7f3190c10a95",
+    "url": "/Portifolio/static/media/11.6a10b1f3.jpeg"
+  },
+  {
+    "revision": "0810f919a56c76e5a6c94b9b845dd095",
+    "url": "/Portifolio/static/media/12.0810f919.jpeg"
+  },
+  {
+    "revision": "e2b27de41d625ea482b0e182917901d3",
+    "url": "/Portifolio/static/media/015.e2b27de4.jpeg"
+  },
+  {
+    "revision": "518ffe4c7a923793e3263472984f878b",
+    "url": "/Portifolio/static/media/exclusive.518ffe4c.jpg"
+  },
+  {
+    "revision": "8e22a21aa2d371b7031c7d16af73352e",
+    "url": "/Portifolio/static/media/faraway.8e22a21a.jpeg"
+  },
+  {
+    "revision": "ae37a758c5c8b72c7ca9f93f12ed1819",
+    "url": "/Portifolio/static/media/imari.ae37a758.jpeg"
+  },
+  {
+    "revision": "96cd349de753cfab2489f627b1af779a",
+    "url": "/Portifolio/static/media/musk.96cd349d.jpeg"
+  },
+  {
+    "revision": "d7e6f3cb7146e5ce8e32a6058a4b2de6",
+    "url": "/Portifolio/static/media/segno.d7e6f3cb.jpeg"
+  },
+  {
+    "revision": "635ba04a00bc95f28a1da7a16a0486c2",
+    "url": "/Portifolio/static/media/surreal.635ba04a.jpeg"
+  },
+  {
+    "revision": "a76efd432075498d01f692f851c6b2c3",
+    "url": "/Portifolio/static/media/victory.a76efd43.jpeg"
+  },
+  {
+    "revision": "301f1359e8bced35f2dfd4c4e0bc5dc5",
+    "url": "/Portifolio/static/media/essentialBlack.301f1359.jpeg"
+  },
+  {
+    "revision": "21bacee311bbae98ab36fdfe8262a16a",
+    "url": "/Portifolio/static/media/1.21bacee3.jpeg"
+  },
+  {
+    "revision": "391321b9b2c495740100a3cc61b63ed2",
+    "url": "/Portifolio/static/media/2.391321b9.jpeg"
+  },
+  {
+    "revision": "43d62daefaf87e6b180a76ec28988992",
+    "url": "/Portifolio/static/media/3.43d62dae.jpeg"
+  },
+  {
+    "revision": "5f268e70229ed0dbfe985cfcf7961f1d",
+    "url": "/Portifolio/static/media/4.5f268e70.jpeg"
+  },
+  {
+    "revision": "9f40b5b524b74c9b80a1ab0fce9c3c22",
+    "url": "/Portifolio/static/media/5.9f40b5b5.jpeg"
+  },
+  {
+    "revision": "f9d7f11488e510628f180954d181a8a1",
+    "url": "/Portifolio/static/media/6.f9d7f114.jpeg"
+  },
+  {
+    "revision": "ef1d86728957e03a0d60122ba60ef829",
+    "url": "/Portifolio/static/media/7.ef1d8672.jpeg"
+  },
+  {
+    "revision": "caf40145333c0b6bba895baa8955bd70",
+    "url": "/Portifolio/static/media/8.caf40145.jpeg"
+  },
+  {
+    "revision": "88a165fb745d28e019ce9d5d07d04f50",
+    "url": "/Portifolio/static/media/9.88a165fb.jpeg"
+  },
+  {
+    "revision": "c93ddbe0f1d3cf239b5278154256110e",
+    "url": "/Portifolio/static/media/1.c93ddbe0.jpeg"
+  },
+  {
+    "revision": "b40c80facde9e89c45980a67b2fc4f9a",
+    "url": "/Portifolio/static/media/2.b40c80fa.jpeg"
+  },
+  {
+    "revision": "7dbe1f8503a9ea0358fddc24ee2b9925",
+    "url": "/Portifolio/static/media/3.7dbe1f85.jpeg"
+  },
+  {
+    "revision": "bae9675364641afc9906a6a0b3f49ebf",
+    "url": "/Portifolio/static/media/4.bae96753.jpeg"
+  },
+  {
+    "revision": "87ed334ce4b3939f8ea7aba1daea771e",
+    "url": "/Portifolio/static/media/5.87ed334c.jpeg"
+  },
+  {
+    "revision": "4cbc19baf338b3b495f1fd8527977003",
+    "url": "/Portifolio/static/media/6.4cbc19ba.jpeg"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/Portifolio/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "f61a4eb27371b7453bf5b12ab3648b9e",
+    "url": "/Portifolio/static/media/Poppins-Medium.f61a4eb2.ttf"
+  },
+  {
+    "revision": "4cdacb8f89d588d69e8570edcbe49507",
+    "url": "/Portifolio/static/media/Poppins-SemiBold.4cdacb8f.ttf"
+  },
+  {
+    "revision": "a3e0b5f427803a187c1b62c5919196aa",
+    "url": "/Portifolio/static/media/Poppins-Bold.a3e0b5f4.ttf"
+  },
+  {
+    "revision": "f6ea751e936ade6edcd03a26b8153b4a",
+    "url": "/Portifolio/static/media/Poppins-Light.f6ea751e.ttf"
+  },
+  {
+    "revision": "25cd0f688f815bc4f6ac2b71eb6278ba",
+    "url": "/Portifolio/static/media/Poppins-Thin.25cd0f68.ttf"
+  },
+  {
+    "revision": "544fa4f2678a8285eb88b8dfe503c90c",
+    "url": "/Portifolio/static/media/Poppins-ExtraBold.544fa4f2.ttf"
+  },
+  {
+    "revision": "bd8f063401e0c603d742c8cd2eeacd5f",
+    "url": "/Portifolio/static/media/nycity.bd8f0634.jpg"
+  },
+  {
+    "revision": "b45917c5df1a0f1ee81078ea40a2d858",
+    "url": "/Portifolio/index.html"
+  }
+];
