e8289873468061d8c5b71a80fbbdc55025d0ccc8
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-11-11 23:29:55 -0300

    Update on index

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement ).render(
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+reportWebVitals();
